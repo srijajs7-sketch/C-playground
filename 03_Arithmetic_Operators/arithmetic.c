ddd49f50e92a89fa0c49be54c9993e14fe16f36b
Srija Js <srijajs7@gmail.com>
2026-07-16 12:02:19 +0530

    Complete arithmetic operations in C

---
+++ 03_Arithmetic_Operators/arithmetic.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+
+    int a = 10;
+    int b = 3;
+
+    // Addition
+    printf("Addition: %d\n", a + b);
+
+    // Subtraction
+    printf("Subtraction: %d\n", a - b);
+
+    // Multiplication
+    printf("Multiplication: %d\n", a * b);
+
+    // Division
+    printf("Division: %d\n", a / b);
+
+    // Modulus (remainder)
+    printf("Remainder: %d\n", a % b);
+
+    return 0;
+}
